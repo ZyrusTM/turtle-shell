33caf294d2e6d00bfc8bbc39db8d1b02107c42ea
DevCyntrix <devcyntrix@hotmail.com>
2023-01-02 13:47:19 +0100

    Add impress site

--- components/Header.tsx
+++ components/Header.tsx
@@ -55,21 +55,21 @@ export default function Header() {
     return (
         <>
             <MantineHeader height={110} className={styles.header}>
-            <h2>Turtle Shell</h2>
+                <h2>Turtle Shell</h2>
 
-            {renderNavigation(true)}
+                {renderNavigation(true)}
 
-            <div className={'search ' + styles.search}>
-            <TextInput icon={<IconSearch />} placeholder="Question" className={styles.searchInput} radius='xl' wrapperProps={{
-                styles: {
-                backgroundColor: 'transparent'
-                }
-            }}/>
-            </div>
-            
-            <div>
-            <Button variant="outline" className={styles.getStartedButton} radius="xl" size="md">Get Started</Button>
-            </div>
-        </MantineHeader>
+                <div className={'search ' + styles.search}>
+                <TextInput icon={<IconSearch />} placeholder="Question" className={styles.searchInput} radius='xl' wrapperProps={{
+                    styles: {
+                    backgroundColor: 'transparent'
+                    }
+                }}/>
+                </div>
+                
+                <div>
+                <Button variant="outline" className={styles.getStartedButton} radius="xl" size="md">Get Started</Button>
+                </div>
+            </MantineHeader>
         </>)
 }
